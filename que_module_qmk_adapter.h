328b0fa1519e238923be8c758e8f3317a413070e
Nshu <d16131@ccst.numazu-ct.ac.jp>
2018-12-11 12:06:32 +0900

    add que_module_qmk_adapter.h

---
+++ que_module_qmk_adapter.h
@@ -0,0 +1,14 @@
+//
+// Created by abata on 2018/12/11.
+//
+
+#ifndef QMK_UNIT_TEST_QUE_MODULE_QMK_ADAPTER_H
+#define QMK_UNIT_TEST_QUE_MODULE_QMK_ADAPTER_H
+
+#include <stdint.h>
+
+#define QUE_SIZE 8
+#define NULL_QUE (255)
+typedef uint8_t data_t;
+
+#endif //QMK_UNIT_TEST_QUE_MODULE_QMK_ADAPTER_H
